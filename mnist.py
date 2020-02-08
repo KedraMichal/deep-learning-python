587477d2192d72e06b98c6ff1b96f52d5e533d1f
ked-m <ked-mic@wp.pl>
2020-02-08 23:51:13 +0100

    number classification

--- mnist.py
+++ mnist.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-mnist = tf.keras.datasets.mnist
 
 np.set_printoptions(suppress=True)
 
-(x_train, y_train), (x_test,y_test) = mnist.load_data()# 60k-10k
-#print(x_train[0])# 784 value are the pixels of the 28x28 image, numbers from 0 to 255 (8bits)
-#print(y_train[0]) # digit which is on the image
+###  Data set contains 60k traning images of numbers from 0 to 9
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-#plt.imshow(x_train[0])
-#plt.show()
+print(x_train[0])  # one x contains 784 values (number of pixels - 28x28 image), from 0 to 255 (8bits)
+print(y_train[0])  # our labels, digit which is on the image from 0 to 9
 
+plt.imshow(x_train[0])
+plt.show()
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
@@ -23,31 +24,25 @@ model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss ='sparse_categorical_crossentropy', metrics=['accuracy'])
-
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
 
 val_loss, val_acc = model.evaluate(x_test, y_test)
 print(val_loss, val_acc)
 
-
 model.save("number_reader")
 new_model = tf.keras.models.load_model('number_reader')
-
 
 predictions = new_model.predict(x_test)
 
 print(predictions.shape)
 
-print(predictions[0,:])
-print(np.argmax(predictions[0]))
 acc = 0
 for i in range(10000):
-    pred = np.argmax(predictions[i,:])
+    pred = np.argmax(predictions[i, :])
     if pred == y_test[i]:
-        acc +=1
+        acc += 1
     print(pred, y_test[i])
 
-
-print(acc/10000)
+print(acc / 10000)
